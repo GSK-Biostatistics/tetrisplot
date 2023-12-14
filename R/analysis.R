5c38c22e35d4d6424dc6f671e8a5b2c1452675b1
Douglas Thompson <dt398512@us1salxhpe0033.corpnet2.com>
2023-12-14 06:18:04 -0500

    Added documentation for the backwards selection analysis function

--- R/analysis.R
+++ R/analysis.R
@@ -1,10 +1,6 @@
+#-------------------------------------------------------------------------------
 #' @title Uni-variate variable selection
 #' @description A univariate screening of covariates against some outcome. 
-#'    Although discouraged (Moons et al., 2012), univariate selection is not 
-#'    uncommon, particularly as a way to pre-screen many covariates prior to 
-#'    some multivariable fit with possible further feature selection thereafter. 
-#'    The method implemented here is a Wald Chi-squared test under an assumption 
-#'    of asymptotic (multivariate) normality.
 #' @param x An output of the `bootstrap_data` function.
 #' @param response a character argument denoting the outcome
 #' @param vars a character vector denoting the covariates or features to be 
@@ -16,16 +12,22 @@
 #' @param method a character value of the chosen test to be used. Currently 
 #'    only a Wald Chi-squared test is implemented.
 #'
-#' @details A number of `analysis_` wrappers have been created to illustrate 
-#'    the challenge of final variable selection. These can be combined as 
-#'    desired by the user.
+#' @details A number of `analysis` wrappers have been created to illustrate 
+#'    the challenge of final variable selection. These can be combined in 
+#'    sequence as desired by the user to consider combinations of selection per 
+#'    bootstrap replication. Although discouraged (Moons et al., 2012), 
+#'    univariate selection is not uncommon, particularly as a way to pre-screen 
+#'    many covariates prior to some multi-variable fit with possible further 
+#'    feature selection thereafter. The present function is a simple wrapper for 
+#'    the `aod::wald.test()` function, an implementation of the Wald Chi-squared 
+#'    test under an assumption of asymptotic (multivariate) normality.
 #' @return A nested `tibble` 
 #' @references 
 #' Moons, K. G. M., Kengne, A. P., Woodward, M., Royston, P., Vergouwe, Y., 
-#' Altman, D. G., & Grobbee, D. E. (2012). Risk prediction models: I. 
-#' Development, internal validation, and assessing the incremental value of a 
-#' new (bio)marker. Heart, 98(9), 683–690. 
-#' https://doi.org/10.1136/heartjnl-2011-301246
+#'    Altman, D. G., & Grobbee, D. E. (2012). Risk prediction models: I. 
+#'    Development, internal validation, and assessing the incremental value of 
+#'    a new (bio)marker. Heart, 98(9), 683–690. 
+#'    https://doi.org/10.1136/heartjnl-2011-301246
 #' @export
 #' @examples
 #' data(iswr_stroke)
@@ -36,7 +38,6 @@
 #'                               "Diabetes", "MI", "Hypertension"),
 #'                      level = 0.05)
 #'
-
 analyse_univariate <- function(x, 
                                response = NULL, 
                                vars = NULL, 
@@ -72,19 +73,20 @@ analyse_univariate <- function(x,
     
     out <- dplyr::mutate(
       .data = x, 
-      vars = purrr::map2(data, 
-                         vars,
-                         function(y, z){
-                           mods_char <- purrr::map(z, ~ glue::glue({response}, " ~ ", {.x}))
-                           names(mods_char) <- z
-                           rslt <- 
-                             purrr::map_chr(mods_char, 
-                                            ~ step_univariate(data = y, 
-                                                              model = .x, 
-                                                              level = level,
-                                                              family = family))
-                           unname(rslt[!is.na(rslt)])
-                         })
+      vars = purrr::map2(
+        data, 
+        vars,
+        function(y, z){
+          mods_char <- purrr::map(z, ~ glue::glue({response}, " ~ ", {.x}))
+          names(mods_char) <- z
+          rslt <- 
+            purrr::map_chr(mods_char, 
+                           ~ step_univariate(data = y, 
+                                             model = .x, 
+                                             level = level,
+                                             family = family))
+          unname(rslt[!is.na(rslt)])
+        })
     )
   }
   
@@ -96,7 +98,44 @@ analyse_univariate <- function(x,
   out
 }
 
+#-------------------------------------------------------------------------------
+#' @title Backward stepwise selection using AIC
+#' @description An application of backward stepwise selection using AIC as the 
+#'    criterion for stepping down (i.e., from a full model) through the 
+#'    candidate set of variables.     
+#' @param x An output of the `bootstrap_data` function.
+#' @param response a character argument denoting the outcome
+#' @param vars a character vector denoting the covariates or features to be 
+#'    screened
+#' @param family the error distribution and link function passed to the `glm()` 
+#'    call inside the `analyse_univariate` function. Default is `gaussian`.
+#' @param k as per the `stats::step` definition: "the multiple of the number of 
+#'    degrees of freedom used for the penalty". 
+#'    
+#' @details The present function is a simple wrapper for the `stats::step()`
+#'    function. There are various commentaries that have been published on the 
+#'    use of stepwise selection, with a range of interpretations of its utility, 
+#'    spanning from more generous proponents focussing on the simplicity with 
+#'    which a reduced model with "insignificant" variables removed, through to 
+#'    those that highlight how it invalidates most of any statistical inference 
+#'    that can be drawn from the final fit. Irrespective of the stance taken, 
+#'    this method is still common practice in many clinical prediction modelling 
+#'    publications and is thus worth including for illustrating selection 
+#'    instability. 
+#' @return A nested `tibble` 
+#' @references 
+#' Harrell, F. E. (2015). Regression Modeling Strategies: With Applications to 
+#'    Linear Models, Logistic and Ordinal Regression, and Survival Analysis. 
+#'    Springer International Publishing. 
+#'    https://books.google.co.uk/books?id=sQ90rgEACAAJ
 #' @export
+#' @example 
+#' data(iswr_stroke)
+#' iswr_stroke %>%
+#'   bootstrap_data(10, seed = 1234) %>%
+#'   analyse_backward(response = "dead12",
+#'                    vars = c("Gender", "Age", "Diagnosis", "Coma",
+#'                             "Diabetes", "MI", "Hypertension"))
 analyse_backward <- function(x, 
                              response = NULL, 
                              vars = NULL, 
@@ -106,12 +145,13 @@ analyse_backward <- function(x,
   if(!any(class(x) == "tetris_analysis")){
     mod_char <- glue::glue(response, " ~ ", paste0(vars, collapse = " + "))
     
-    out <- dplyr::mutate(.data = x, 
-                         vars = purrr::map(data, 
-                                           ~ step_backward(data = .x, 
-                                                           model = mod_char, 
-                                                           family = family, 
-                                                           k = k)))
+    out <- dplyr::mutate(
+      .data = x, 
+      vars = purrr::map(data, 
+                        ~ step_backward(data = .x, 
+                                        model = mod_char, 
+                                        family = family, 
+                                        k = k)))
   }
   
   if(any(class(x) == "tetris_analysis")){
@@ -120,15 +160,16 @@ analyse_backward <- function(x,
     
     out <- dplyr::mutate(
       .data = x, 
-      vars = purrr::map2(data, 
-                         vars,
-                         function(y, z){
-                           mod_char <- glue::glue(response, " ~ ", paste0(z, collapse = " + "))
-                           step_backward(data = y, 
-                                         model = mod_char, 
-                                         family = family, 
-                                         k = k) 
-                         }))
+      vars = purrr::map2(
+        data, 
+        vars,
+        function(y, z){
+          mod_char <- glue::glue(response, " ~ ", paste0(z, collapse = " + "))
+          step_backward(data = y, 
+                        model = mod_char, 
+                        family = family, 
+                        k = k) 
+        }))
   }
   
   if(!is.null(vars))
@@ -139,11 +180,7 @@ analyse_backward <- function(x,
   out
 }
 
-# stepwise_forward
-# 
-# stepwise_both
-# 
-
+#-------------------------------------------------------------------------------
 #' @export
 analyse_grlasso <- function(x, 
                             response = NULL, 
@@ -154,12 +191,13 @@ analyse_grlasso <- function(x,
   if(!any(class(x) == "tetris_analysis")){
     mod_char <- glue::glue(response, " ~ ", paste0(vars, collapse = " + "))
     
-    out <- dplyr::mutate(.data = x, 
-                         vars = purrr::map(data, 
-                                           ~ step_grlasso(data = .x, 
-                                                          model = mod_char, 
-                                                          family = family, 
-                                                          grpreg_list = grpreg_list)))
+    out <- dplyr::mutate(
+      .data = x, 
+      vars = purrr::map(data, 
+                        ~ step_grlasso(data = .x, 
+                                       model = mod_char, 
+                                       family = family, 
+                                       grpreg_list = grpreg_list)))
   }
   
   if(any(class(x) == "tetris_analysis")){
@@ -168,15 +206,16 @@ analyse_grlasso <- function(x,
     
     out <- dplyr::mutate(
       .data = x, 
-      vars = purrr::map2(data, 
-                         vars,
-                         function(y, z){
-                           mod_char <- glue::glue(response, " ~ ", paste0(z, collapse = " + "))
-                           step_grlasso(data = .x, 
-                                        model = mod_char, 
-                                        family = family, 
-                                        grpreg_list = grpreg_list)
-                         }))
+      vars = purrr::map2(
+        data, 
+        vars,
+        function(y, z){
+          mod_char <- glue::glue(response, " ~ ", paste0(z, collapse = " + "))
+          step_grlasso(data = .x, 
+                       model = mod_char, 
+                       family = family, 
+                       grpreg_list = grpreg_list)
+        }))
   }
   
   if(!is.null(vars))
